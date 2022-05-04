b95d05a1208c95b7b29385aede25f19401f56212
shijoshaji <jojovms@gmail.com>
2022-05-04 22:41:49 +0530

    Function to fetch API
    Fetch API and display the result in front end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherInterface } from './models/weatherAPI.models';
+import { WeatherAPIService } from './services/weather-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'weatherApp';
+export class AppComponent implements OnInit {
+  constructor(private weatherService: WeatherAPIService) {}
+
+  weatherData?: WeatherInterface;
+
+  ngOnInit(): void {
+    this.weatherService.fetchWeatherData('London').subscribe({
+      next: (response) => {
+        this.weatherData = response;
+      },
+    });
+  }
 }
