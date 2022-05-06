a34527ce4d6668bd6fa10b47d06c11dac84de69d
shijoshaji <jojovms@gmail.com>
2022-05-06 23:33:09 +0530

    Angular Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  baseURL: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  reverseGeoCodeURL:
+    'https://api.bigdatacloud.net/data/reverse-geocode-client?',
+  XRapidAPIHostName: 'X-RapidAPI-Host',
+  XRapidAPIHostValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyName: 'X-RapidAPI-Key',
+  XRapidAPIKeyValue: 'YOUR_KEY_VALUE',
 };
